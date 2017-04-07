99aa3abc28fd8acfbabba2d550f9da9c7d3d2ab0
gaochundong <gaochundong@hotmail.com>
2017-04-07 15:13:57 +0800

    just rename the longid name

--- Issuna/Issuna.Core/LongId.cs
+++ Issuna/Issuna.Core/LongId.cs
@@ -8,8 +8,7 @@ namespace Issuna.Core
 {
     /// <summary>
     /// LongId is a 64-bit(long/int64) ID consists of:
-    /// 63-bit as R-bit, reserved for extension, 0 by default.
-    /// 62-61-bit as S-bits, 2 bits represents service model, 0 by default.
+    /// 63-61-bit as R-bit, 3 bits reserved for extensions, 0 by default.
     /// 60-51-bit as M-bits, 10 bits represents machine identifier, 10 bits covers 1-1024 servers.
     /// 50-bit as P-bit, time precision for using second(0) or millisecond(1), 0 by default.
     /// 49-20-bit as T-bits, timestamp if the P-bit is second(0), 30 bits covers more than 30 years.
