a80496a933e77092513ae629781067d17f1de010
gaochundong <gaochundong@hotmail.com>
2017-04-07 21:16:21 +0800

    reference to happer to prepare to add rest service

--- Issuna/Issuna.RestService/Program.cs
+++ Issuna/Issuna.RestService/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace Issuna.RestService
+﻿using System;
+using Happer;
+using Happer.Hosting.Self;
+using Logrila.Logging;
+using Logrila.Logging.NLogIntegration;
+
+namespace Issuna.RestService
 {
     class Program
     {
         static void Main(string[] args)
         {
+            NLogLogger.Use();
+
+            ILog log = Logger.Get<Program>();
+
+            var container = new ModuleContainer();
+            //container.AddModule(new TestModule());
+
+            var bootstrapper = new Bootstrapper();
+            var engine = bootstrapper.BootWith(container);
+
+            string uri = "http://localhost:3202/";
+            var host = new SelfHost(engine, new Uri(uri));
+            host.Start();
+            Console.WriteLine("RestService is listening on [{0}].", uri);
+
+            Console.WriteLine("Type something to stop ...");
+            Console.ReadKey();
+
+            host.Stop();
+            Console.WriteLine("Stopped. Goodbye!");
         }
     }
 }
