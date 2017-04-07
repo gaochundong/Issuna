ef54209624e080a5be0fbf77528999ddfe36b7cd
gaochundong <gaochundong@hotmail.com>
2017-04-07 19:07:17 +0800

    adding unit tests for objectid

--- Issuna/Issuna.Core/Properties/AssemblyInfo.cs
+++ Issuna/Issuna.Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,8 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Issuna.Core")]
 [assembly: Guid("55a6c733-3ebe-4abe-bee1-78c506838160")]
+
+[assembly: InternalsVisibleTo("Issuna.Core.Tests")]
